f4c5653ef810afac34e05625f5e218b92ca7b171
isaacloveschicken <isaacww@byu.edu>
2020-02-13 11:50:52 -0700

    updated settings

--- server/_config/config.js
+++ server/_config/config.js
@@ -3,12 +3,12 @@ const testDbName = 'todo-test'
 
 export const configOptions = {
   development: {
-    db: `mongodb://localhost/${dbName}`,
+    db: `mongodb://IWW:4Fatmantarays@cluster0-shard-00-00-kfesp.mongodb.net:27017,cluster0-shard-00-01-kfesp.mongodb.net:27017,cluster0-shard-00-02-kfesp.mongodb.net:27017/${dbName}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
     port: process.env.PORT || 3001,
     userCookieName: 'user'
   },
   testDevelopment: {
-    db: `mongodb://localhost/${testDbName}`,
+    db: `mongodb://IWW:4Fatmantarays@cluster0-shard-00-00-kfesp.mongodb.net:27017,cluster0-shard-00-01-kfesp.mongodb.net:27017,cluster0-shard-00-02-kfesp.mongodb.net:27017/${testDbName}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
     port: process.env.PORT || 3001,
     userCookieName: 'user'
   },
