39e901e6822f2a6c28469339e90031544138d49c
isaacloveschicken <isaacww@byu.edu>
2020-02-13 01:29:39 -0700

    updated urls to project

--- server/_config/config.js
+++ server/_config/config.js
@@ -13,12 +13,12 @@ export const configOptions = {
     userCookieName: 'user'
   },
   production: {
-    db: `mongodb://pdrean4:willchangesoon@cluster0-shard-00-00-pg36l.mongodb.net:27017,cluster0-shard-00-01-pg36l.mongodb.net:27017,cluster0-shard-00-02-pg36l.mongodb.net:27017/${dbName}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
+    db: `mongodb://IWW:4Fatmantarays@cluster0-shard-00-00-kfesp.mongodb.net:27017,cluster0-shard-00-01-kfesp.mongodb.net:27017,cluster0-shard-00-02-kfesp.mongodb.net:27017/${dbName}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
     port: process.env.PORT || 3001,
     userCookieName: 'user'
   },
   testProduction: {
-    db: `mongodb://pdrean4:willchangesoon@cluster0-shard-00-00-pg36l.mongodb.net:27017,cluster0-shard-00-01-pg36l.mongodb.net:27017,cluster0-shard-00-02-pg36l.mongodb.net:27017/${testDbName}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
+    db: `mongodb://IWW:4Fatmantarays@cluster0-shard-00-00-kfesp.mongodb.net:27017,cluster0-shard-00-01-kfesp.mongodb.net:27017,cluster0-shard-00-02-kfesp.mongodb.net:27017/${testDbName}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
     port: process.env.PORT || 3001,
     userCookieName: 'user'
   }
